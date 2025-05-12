2a332a3ae7b47f68b21befb0cd82e7a8442414c4
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-12 09:41:06 -0400

    Type Casting

--- Type Error, Checking and Conversion/task.py
+++ Type Error, Checking and Conversion/task.py
@@ -1,5 +1,9 @@
-len("12345")
-print(type("hi"))
-print(type(123))
-print(type(1.1234))
-print(type(True))
+# len("12345")
+# print(type("hi"))
+# print(type(123))
+# print(type(1.1234))
+# print(type(True))
+
+print(int("123") + int("456"))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
