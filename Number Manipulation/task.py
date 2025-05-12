e67cbbfe07b37c6a0afac25d3d5a0f7b23602f89
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-12 10:03:15 -0400

    Number Manipulation and Formatted Strings

--- Number Manipulation/task.py
+++ Number Manipulation/task.py
@@ -1,1 +1,16 @@
 bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi))
+
+print(round(bmi))
+
+print(round(bmi, 3))
+
+score = 0
+print(score)
+score += 1
+print(score)
+is_winning = False
+
+print(f"Your score is: {score}! Are you in 1st place? {is_winning}")
