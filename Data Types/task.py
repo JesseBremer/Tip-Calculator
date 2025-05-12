bac6ab9a2acba25f169e4dc64e0ec7a90dd7a515
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-12 09:19:45 -0400

    Data Types

--- Data Types/task.py
+++ Data Types/task.py
@@ -1,1 +1,17 @@
-len("Hello")
+#Subscriptions
+from xmlrpc.client import Boolean
+
+print("Hello"[-1])
+
+# Integers
+print(123 + 345)
+
+# Large Integers
+print(123_456_789)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
