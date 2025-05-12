42248ae8c587a8491aa7f872a5c2d4b57ef863e5
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-12 09:49:49 -0400

    Mathematical Operators

--- Mathematical Operations/task.py
+++ Mathematical Operations/task.py
@@ -1,1 +1,14 @@
 print("My age: " + str(12))
+print(123 + 346)
+print(10 - 5)
+print(10 + 7)
+print(4 * 4)
+print(5 / 4)
+print(5 // 4)
+print(3 ** 2)
+
+# 7
+print(3 * 3 + 3 / 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
+
